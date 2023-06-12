28c3e58771adb20224fe6ccde196816bcfe8c2f1
Luisitodelcastillo <castilloluis166@gmail.com>
2023-06-12 17:07:13 +0200

    Catalag: Actualizar productos

--- Botiga_grupC/cataleg/urls.py
+++ Botiga_grupC/cataleg/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/add/', views.add_product, name="Añadir Producto")
+    path('products/add/', views.add_product, name="Añadir Producto"),
+    path('products/update/<int:pk>', views.update_product, name = "Actualizar Producto")
 ]
--- Botiga_grupC/cataleg/views.py
+++ Botiga_grupC/cataleg/views.py
@@ -3,6 +3,9 @@ from rest_framework.decorators import api_view
 from rest_framework.response import  Response
 from .models import Producto
 from .serializer import ProductoSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+
 
 # Create your views here.
 
@@ -13,3 +16,19 @@ def add_product(request):
         serializer.save()
         return Response(serializer.data, status=201)
     return  Response(serializer.errors, status=400)
+
+@api_view(['GET','PUT'])
+def update_product(request, pk):
+    try:
+        product = Producto.objects.get(pk=pk)
+    except Producto.DoesNotExist:
+        return Response({'error':'Producto no encontrado'}, status=404)
+    if request.method == 'PUT':
+        serializer = ProductoSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+    elif request.method == 'GET':
+        serializer = ProductoSerializer(product)
+        return Response(serializer.data)
